7e4ac77d2a29a3a2c6f87f2f92576ce88d31fe95
Salas\Jorge Salazar <jorsala01@gmail.com>
2017-06-08 14:32:05 -0600

    Este es el primer Commit

---
+++ HojaTrab1/src/hojatrab1/HojaTrab1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hojatrab1;
+
+/**
+ *
+ * @author Jorge Salazar
+ */
+public class HojaTrab1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     Hoja ho = new Hoja();
+     ho.setVisible(true);
+        // TODO code application logic here
+    }
+    
+}
